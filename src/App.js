f85a2525a4854d3e47a259d748cfd9b2f50aec21
ADIL <115420728+adil-hussa1n@users.noreply.github.com>
2024-10-16 20:54:29 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react'; 
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
@@ -8,18 +9,32 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import { About } from "./components/About";
-
+import Preloader from './components/Preloader';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); 
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <NavBar />
-      <Banner />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+      {loading ? <Preloader /> : (
+        <>
+          <NavBar />
+          <Banner />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Preloader.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './Preloader.css';
+
+function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="loader">
+        <h1 className="letter text-secondary animate">G</h1>
+        <h1 className="letter text-tertiary animate">r</h1>
+        <h1 className="letter text-secondary animate">a</h1>
+        <h1 className="letter text-tertiary animate">f</h1>
+        <h1 className="letter text-secondary animate">f</h1>
+        <h1 className="letter text-tertiary animate">i</h1>
+        <h1 className="letter text-secondary animate">x</h1>
+        <h1 className="letter text-tertiary animate">I</h1>
+        <h1 className="letter text-secondary animate">n</h1>
+        <h1 className="letter text-tertiary animate">n</h1>
+        <h1 className="letter text-secondary animate">o</h1>
+        <h1 className="letter text-tertiary animate">v</h1>
+        <h1 className="letter text-secondary animate">a</h1>
+        <h1 className="letter text-tertiary animate">t</h1>
+        <h1 className="letter text-secondary animate">i</h1>
+        <h1 className="letter text-tertiary animate">o</h1>
+        <h1 className="letter text-tertiary animate">n</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
