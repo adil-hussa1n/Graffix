05d710176f8ceac1ce2bc29cb3373e977c9ebe3d
ADIL <115420728+adil-hussa1n@users.noreply.github.com>
2024-10-16 21:37:05 +0600

    .

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -5,23 +5,23 @@ function Loader() {
   return (
     <div className="loader-container">
       <div className="loader">
-        <h1 className="letter text-secondary animate">G</h1>
-        <h1 className="letter text-tertiary animate">r</h1>
-        <h1 className="letter text-secondary animate">a</h1>
-        <h1 className="letter text-tertiary animate">f</h1>
-        <h1 className="letter text-secondary animate">f</h1>
-        <h1 className="letter text-tertiary animate">i</h1>
-        <h1 className="letter text-secondary animate">x</h1>
-        <h1 className="letter text-tertiary animate">I</h1>
-        <h1 className="letter text-secondary animate">n</h1>
-        <h1 className="letter text-tertiary animate">n</h1>
-        <h1 className="letter text-secondary animate">o</h1>
-        <h1 className="letter text-tertiary animate">v</h1>
-        <h1 className="letter text-secondary animate">a</h1>
-        <h1 className="letter text-tertiary animate">t</h1>
-        <h1 className="letter text-secondary animate">i</h1>
-        <h1 className="letter text-tertiary animate">o</h1>
-        <h1 className="letter text-tertiary animate">n</h1>
+        <h1 className="letter text-white animate">G</h1>
+        <h1 className="letter text-white animate">r</h1>
+        <h1 className="letter text-white animate">a</h1>
+        <h1 className="letter text-white animate">f</h1>
+        <h1 className="letter text-white animate">f</h1>
+        <h1 style={{color: "#f39c12"}} className="letter animate">i</h1>
+        <h1 className="letter text-white animate">x</h1>
+        <h1 className="letter text-white animate">I</h1>
+        <h1 className="letter text-white animate">n</h1>
+        <h1 className="letter text-white animate">n</h1>
+        <h1 className="letter text-white animate">o</h1>
+        <h1 className="letter text-white animate">v</h1>
+        <h1 className="letter text-white animate">a</h1>
+        <h1 className="letter text-white animate">t</h1>
+        <h1 className="letter text-white animate">i</h1>
+        <h1 className="letter text-white animate">o</h1>
+        <h1 className="letter text-white animate">n</h1>
       </div>
     </div>
   );
